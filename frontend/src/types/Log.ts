9c88bd6dc00a6fde6e63d77221e00bdd325b1aba
Unknownflow <clarencelau88@gmail.com>
2026-06-02 16:28:25 +0800

    refactor: move all types relating to logs into a single file

---
+++ frontend/src/types/Log.ts
@@ -0,0 +1,32 @@
+export type LogType = "debug" | "info" | "warning" | "error";
+
+export type KeyValue = {
+    key: string;
+    value: string;
+};
+
+export type LogRecord = {
+    timestamp: Date;
+    traceId: string;
+    spanId: string;
+    severityText: string;
+    severityNumber: number;
+    body: string;
+    logAttributes: KeyValue[];
+    resourceAttributes: KeyValue[];
+};
+
+export type LogRecordDTO = {
+    timestamp: Date;
+    traceId: string;
+    spanId: string;
+    severityText: LogType;
+    severityNumber: number;
+    body: string;
+    logAttributes: KeyValue[];
+};
+export type LogIngestRequest = {
+    serviceName: string;
+    resourceAttributes: KeyValue[];
+    records: LogRecordDTO[];
+};
--- frontend/src/types/LogType.ts
+++
@@ -1,1 +0,0 @@
-export type LogType = "debug" | "info" | "warning" | "error";
