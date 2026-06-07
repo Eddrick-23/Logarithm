bef0afe163b1579c0415049a5114afdaf4c54d6e
Eddrick <eddricklivando@gmail.com>
2026-06-07 12:58:18 +0800

    feat: update log ingestion routes for OTLP handling. - no more partial decode - pipe straight to nats to handle filtering and flattening later

--- backend/api/schemas/logs.go
+++ backend/api/schemas/logs.go
@@ -9,7 +9,7 @@ type KeyValue struct {
 }
 
 // LogRecordDTO represents the individual record payload from the frontend/client.
-type LogRecordDTO struct {
+type LogRecordDTO struct { //TODO remove after migration
 	Timestamp      time.Time  `json:"timestamp"`
 	TraceId        string     `json:"traceId"`
 	SpanId         string     `json:"spanId"`
@@ -23,7 +23,7 @@ type LogRecordDTO struct {
 LogIngestRequest handles batch logs sent by clients.
 Includes ServiceName and ResourceAttributes once in the grouped JSON.
 */
-type LogIngestRequest struct {
+type LogIngestRequest struct { //TODO remove after migration
 	ServiceName        string         `json:"serviceName"`
 	ResourceAttributes []KeyValue     `json:"resourceAttributes,omitempty"`
 	Records            []LogRecordDTO `json:"records"`
