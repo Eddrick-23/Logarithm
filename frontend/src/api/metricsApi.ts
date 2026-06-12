f333f226e2442983c6ef49135e5f3a378f23c6ee
Unknownflow <clarencelau88@gmail.com>
2026-06-12 17:27:59 +0800

    feat: update frontend to display error rate and error count within the last hour

--- frontend/src/api/metricsApi.ts
+++ frontend/src/api/metricsApi.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
-import type { IngestionMetricsMap, GetIngestionMetricsResponse } from "../types/Metric";
+import type { IngestionMetricsMap, GetIngestionMetricsResponse, ErrorMetrics } from "../types/Metric";
 
 export const fetchIngestionMetrics = async (): Promise<IngestionMetricsMap> => {
     // response is of the type { metrics: IngestionMetricsMap }
     const response = await axios.get<GetIngestionMetricsResponse>("/api/ingestion-metrics");
     return response.data.metrics;
+};
+
+export const fetchErrorMetrics = async (): Promise<ErrorMetrics[]> => {
+    const response = await axios.get<ErrorMetrics[]>("/api/error-metrics");
+    return response.data;
 };
--- frontend/src/hooks/useMetrics.ts
+++ frontend/src/hooks/useMetrics.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchIngestionMetrics } from "../api/metricsApi";
+import { fetchErrorMetrics, fetchIngestionMetrics } from "../api/metricsApi";
 
 export const useIngestionMetrics = () => {
     return useQuery({
@@ -7,5 +7,14 @@ export const useIngestionMetrics = () => {
         queryFn: fetchIngestionMetrics,
         staleTime: 1000 * 60, // 1 minute
         refetchInterval: 1000 * 5, // refetch every 5 seconds
+    });
+};
+
+export const useErrorMetrics = () => {
+    return useQuery({
+        queryKey: ["errorMetrics"],
+        queryFn: fetchErrorMetrics,
+        staleTime: 1000 * 60, // 1 minute
+        refetchInterval: 1000 * 60, // refetch every 1 minute
     });
 };
