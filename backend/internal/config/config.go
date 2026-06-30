796b68b7457c1242df84d9ca0709a6dfa6cdf475
Unknownflow <clarencelau88@gmail.com>
2026-06-30 20:39:59 +0800

    refactor: move format functions into a new file

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -3,8 +3,6 @@ package config
 import (
 	"context"
 	"fmt"
-	"math"
-	"strings"
 	"time"
 
 	"github.com/docker/go-units"
@@ -114,47 +112,6 @@ func (c *Config) validate() error {
 		)
 	}
 	return nil
-}
-
-func formatDuration(d time.Duration) string {
-	// custom duration formatter since package conversion function returns 1h0m0s instead of 1h
-	if d == 0 {
-		return "0s"
-	}
-
-	var b strings.Builder
-
-	hours := int64(d / time.Hour)
-	d -= time.Duration(hours) * time.Hour
-
-	minutes := int64(d / time.Minute)
-	d -= time.Duration(minutes) * time.Minute
-
-	seconds := d.Seconds() // remaining, may be fractional
-
-	if hours > 0 {
-		fmt.Fprintf(&b, "%dh", hours)
-	}
-	if minutes > 0 {
-		fmt.Fprintf(&b, "%dmin", minutes)
-	}
-	if seconds > 0 {
-		if seconds == math.Trunc(seconds) {
-			fmt.Fprintf(&b, "%ds", int64(seconds))
-		} else {
-			fmt.Fprintf(&b, "%gs", seconds)
-		}
-	}
-
-	return b.String()
-}
-
-func formatBackoff(durations []time.Duration) string {
-	parts := make([]string, len(durations))
-	for i, d := range durations {
-		parts[i] = formatDuration(d)
-	}
-	return strings.Join(parts, ", ")
 }
 
 func (c *Config) Public() PublicConfig {
